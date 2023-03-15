362323163b44f1c8c56c496c7962f1d5fb1fb75e
Gyrkora <giselle.gallardo0210@gmail.com>
2023-03-15 18:55:31 -0300

    starting the ui

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+//variables
 
+let btn1 = document.querySelector('.btn1');
+let displayer = document.querySelector('.displayer');
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function operate(operator, num1, num2) {
+	if (operator === '+') {
+		return add(num1, num2);
+	} else if (operator === '-') {
+		return subtract(num1, num2);
+	} else if (operator === '/') {
+		return divide(num1, num2);
+	} else if (operator === '*') {
+		return multiply(num1, num2);
+	} else {
+		return console.log('not a number');
+	}
+}
+
+let OperationResult = operate(`*`, 5, 3);
+
+btn1.addEventListener('click', function hola(params) {
+	console.log(OperationResult);
+});
+
+displayer.textContent = `a text ${OperationResult}`;
