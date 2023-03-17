1a5ec8b421dd9fa22ee978dfa35a39e19b551ab9
Gyrkora <giselle.gallardo0210@gmail.com>
2023-03-17 18:04:26 -0300

    general changes

--- js/main.js
+++ js/main.js
@@ -1,84 +1,48 @@
+//the functions add, sub, multiply, divide
+let currentValue = '';
+let currentOperation = '';
 
+function add(num1, num2) {
+	return `num1 ${'+'} num2`;
+}
 
+function subtract(num1, num2) {
+	return 'num1 - num2';
+}
 
+function divide(num1, num2) {
+	return 'num1 / num2';
+}
 
+function multiply(num1, num2) {
+	return 'num1 * num2';
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function add(a,b){
-//   let c = a+b
-//     return c
-// }
-//  console.log(add(1,4))
-// function subtract(a,b){
-//     let c = a-b
-//     return c
-// }
-// subtract(2,4)
-// function multiply(a,b){
-//     let c = a*b
-//     return c
-// }
-// console.log(multiply(2,4))
-// function divide(a,b){
-//     let c = a/b
-//     return c
-// }
-// console.log(divide(2,4))
- 
-// function operate(opperator,num1,num2){
-//     let result
-//     if(opperator==="add"){
-//         result = add(num1,num2)
-//         return result
-//     }
-//     else if(opperator=="sub"){
-//         result = subtract(num1,num2)
-//         return result
-//     }
-//    else if(opperator=="mult"){
-//         result = multiply(num1,num2)
-//         return result
-//    }
-//    else if(opperator=="divi"){
-//         result = subtract(num1,num2)
-//         return result
-//    }
-//    else{
-//     return  null
-//    }
-// }
-
-
+let result;
+function operate(opperator) {
+	if (opperator === '+') {
+		result = add(num1, num2);
+		return +result;
+	} else if (opperator === '-') {
+		result = subtract(num1, num2);
+		return +result;
+	} else if (opperator === '*') {
+		result = multiply(num1, num2);
+		return +result;
+	} else if (opperator === '/') {
+		result = subtract(num1, num2);
+		return +result;
+	} else {
+		return null;
+	}
+}
 
 // const buttons = document.querySelectorAll('button')
 // console.log(buttons[4].innerHTML)
 
 // buttons.forEach((button)=>{
 //     button.addEventListener('click',display(button.innerHTML))
-    
+
 // })
 // function display(value){
 //     let display = document.querySelector('.displayer')
@@ -86,195 +50,41 @@
 //     console.log(value)
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const display = document.querySelector(".displayer");
-// const buttons = document.querySelectorAll("button");
-
-// buttons.forEach((item) => {
-//   item.onclick = () => {
-//     if (item.id == "clear") {
-//       display.innerText = "";
-//     } else if (item.id == "backspace") {
-//       let string = display.innerText.toString();
-//       display.innerText = string.substr(0, string.length - 1);
-//     } else if (display.innerText != "" && item.id == "equal") {
-//       display.innerText = eval(display.innerText);
-//     } else if (display.innerText == "" && item.id == "equal") {
-//       display.innerText = "Empty!";
-//       setTimeout(() => (display.innerText = ""), 2000);
-//     } else {
-//       display.innerText += item.id;
-//     }
-//   };
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("hello world")
-// // let keys = document.querySelectorAll('button')
-// // console.log(keys)
-// // let display = document.querySelector('.displayer')
-// // console.log(display)
-// // display.appendChild()
-// // Initialize variables
+const display = document.querySelector('.displayer');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+	item.onclick = () => {
+		if (item.id == 'clear') {
+			display.innerText = '';
+		} else if (item.id == 'backspace') {
+			let string = display.innerText.toString();
+			display.innerText = string.substr(0, string.length - 1);
+		} else if (display.innerText != '' && item.id == '=') {
+			//equal function - it gives the result
+			display.innerText = operate();
+			if (item.id === '+') {
+				operate('+');
+			}
+		} else if (display.innerText == '' && item.id == '=') {
+			display.innerText = 'Empty!';
+			setTimeout(() => (display.innerText = ''), 2000);
+		} else {
+			display.innerText += new String(item.id);
+		}
+	};
+});
+
+// if (action === 'decimal') {
+// 	display.textContent = displayedNum + '.'
+//   }
+
+// let keys = document.querySelectorAll('button')
+// console.log(keys)
+// let display = document.querySelector('.displayer')
+// console.log(display)
+// display.appendChild()
+// Initialize variables
 // let num1 = '', num2 = '', operator = '';
 
 // // Function to update the screen
@@ -336,61 +146,6 @@
 //   num2 = '';
 //   operator = '';
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function add(num1, num2) {
-// 	return num1 + num2;
-// }
-
-// function subtract(num1, num2) {
-// 	return num1 - num2;
-// }
-
-// function divide(num1, num2) {
-// 	return num1 / num2;
-// }
-
-// function multiply(num1, num2) {
-// 	return num1 * num2;
-// }
-
-// function operate(operator, num1, num2) {
-// 	if (operator === '+') {
-// 		return add(num1, num2);
-// 	} else if (operator === '-') {
-// 		return subtract(num1, num2);
-// 	} else if (operator === '/') {
-// 		return divide(num1, num2);
-// 	} else if (operator === '*') {
-// 		return multiply(num1, num2);
-// 	} else {
-// 		return console.log('not a number');
-// 	}
-// }
-
-// let OperationResult = operate(`*`, 5, 3);
 
 // btn1.addEventListener('click', function hola(params) {
 // 	console.log(OperationResult);
