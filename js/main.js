d3363715ce2cbbe3ccbe11d4f9ccfac345590420
Gyrkora <giselle.gallardo0210@gmail.com>
2023-03-18 05:12:06 -0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//the functions add, sub, multiply, divide
 let firstValue = '';
 let secondValue = '';
 let operation = '';
@@ -16,24 +15,28 @@ function subtract(num1, num2) {
 }
 
 function divide(num1, num2) {
-	return num2 / num1;
+	if (num2 === 0) {
+		return 'Error: impossible operation';
+	} else {
+		return num1 / num2;
+	}
 }
 
 function multiply(num1, num2) {
 	return num1 * num2;
 }
 
-function operate(opperator, a, b) {
-	if (opperator === '+') {
+function operate(operator, a, b) {
+	if (operator === '+') {
 		return add(a, b);
-	} else if (opperator === '-') {
+	} else if (operator === '-') {
 		return subtract(a, b);
-	} else if (opperator === '*') {
+	} else if (operator === '*') {
 		return multiply(a, b);
-	} else if (opperator === '/') {
-		return subtract(a, b);
+	} else if (operator === '/') {
+		return divide(a, b);
 	} else {
-		return 'Error:invalid operator';
+		return 'Error: invalid operator';
 	}
 }
 
@@ -42,55 +45,81 @@ function clear() {
 	firstValue = '';
 	secondValue = '';
 }
+
 function backSpace() {
 	let contentDisplayed = display.innerText.toString();
 	display.innerText = contentDisplayed.substr(0, contentDisplayed.length - 1);
-	// secondValue = secondValue.substr(0, secondValue.length - 1);
+	firstValue = firstValue.substr(0, firstValue.length - 1);
 }
+
+function calculate() {
+	if (secondValue && firstValue && operation) {
+		result = operate(operation, Number(secondValue), Number(firstValue));
+		display.textContent =
+			Math.round((result + Number.EPSILON) * 100000000) / 100000000;
+		firstValue = result.toString();
+		secondValue = '';
+		operation = '';
+	}
+}
+
+function handleInput(input) {
+	if (input === 'clear') {
+		clear();
+	} else if (input === 'backspace') {
+		backSpace();
+	} else if (display.innerText == '' && input === '=') {
+		display.textContent = 'Empty!';
+		setTimeout(() => (display.textContent = ''), 2000);
+	} else {
+		if (input >= '0' && input <= '9') {
+			display.textContent += input;
+			firstValue += input;
+		} else if (
+			input === '+' ||
+			input === '-' ||
+			input === '*' ||
+			input === '/'
+		) {
+			if (operation !== '' && firstValue !== '') {
+				calculate();
+			}
+			operation = input;
+			display.innerText += input;
+			secondValue = firstValue;
+			firstValue = '';
+		} else if (input === '=') {
+			calculate();
+		} else if (input === '.') {
+			if (!firstValue.includes('.')) {
+				display.textContent += input;
+				firstValue += input;
+			}
+		}
+	}
+}
+
+const numbers = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'+',
+	'-',
+	'.',
+	'/',
+	'*',
+];
 
 buttons.forEach((item) => {
 	item.onclick = () => {
-		if (item.id == 'clear') {
-			clear();
-		} else if (item.id == 'backspace') {
-			backSpace();
-		} else if (display.innerText == '' && item.id == '=') {
-			display.textContent = 'Empty!';
-			setTimeout(() => (display.textContent = ''), 2000);
-		} else if (item.id === '.') {
-			display.textContent += item.id;
-		} else {
-			if (item.id >= '0' && item.id <= '9') {
-				display.textContent += item.id;
-				firstValue += item.id;
-			}
-			if (
-				item.id === '+' ||
-				item.id === '-' ||
-				item.id === '*' ||
-				item.id === '/'
-				// ||
-				// item.id === '='
-			) {
-				operation = item.id;
-				display.innerText += item.id;
-				secondValue = firstValue;
-				firstValue = '';
-			}
-
-			if (item.id === '=') {
-				result = operate(operation, Number(firstValue), Number(secondValue));
-				display.textContent = result;
-				console.log(
-					`first value = ${firstValue} ${operation} second value = ${secondValue}`
-				);
-				console.log(`the result = ${result}`);
-				if (secondValue && firstValue && operation) {
-					firstValue = result.toString();
-					secondValue = '';
-				}
-			}
-		}
+		handleInput(item.id);
 	};
 });
 
@@ -99,105 +128,9 @@ window.addEventListener('keydown', function keyPressed(e) {
 		return;
 	}
 
-	const numbers = [
-		'0',
-		'1',
-		'2',
-		'3',
-		'4',
-		'5',
-		'6',
-		'7',
-		'8',
-		'9',
-		'+',
-		'-',
-		'.',
-		'/',
-		'*',
-	];
+	const input = e.key;
 
-	if (numbers.includes(e.key)) {
-		display.textContent += e.key;
-	} else if (e.key == 'Backspace') {
-		backSpace();
+	if (numbers.includes(input)) {
+		handleInput(input);
 	}
 });
-
-// if (action === 'decimal') {
-// 	display.textContent = displayedNum + '.'
-//   }
-
-// let keys = document.querySelectorAll('button')
-// console.log(keys)
-// let display = document.querySelector('.displayer')
-// console.log(display)
-// display.appendChild()
-// Initialize variables
-// let num1 = '', num2 = '', operator = '';
-
-// // Function to update the screen
-// function updateScreen(value) {
-//   document.getElementById('.displayer').value = value;
-// }
-
-// // Function to clear the screen
-// function clearScreen() {
-//   num1 = '';
-//   num2 = '';
-//   operator = '';
-//   updateScreen('');
-// }
-
-// // Function to handle number button clicks
-// function numberPressed(number) {
-//   if (operator === '') {
-//     num1 += number;
-//     updateScreen(num1);
-//   }
-//   else {
-//     num2 += number;
-//     updateScreen(num2);
-//   }
-// }
-
-// // Function to handle operation button clicks
-// function operationPressed(op) {
-//   operator = op;
-// }
-
-// // Function to calculate the results and display them on the screen
-// function calculate() {
-//   let result;
-//   switch (operator) {
-//     case '+':
-//       result = parseFloat(num1) + parseFloat(num2);
-//       break;
-//     case '-':
-//       result = parseFloat(num1) - parseFloat(num2);
-//       break;
-//     case '*':
-//       result = parseFloat(num1) * parseFloat(num2);
-//       break;
-//     case '/':
-//       if (num2 === '0') {
-//         updateScreen('Error');
-//         return;
-//       }
-//       result = parseFloat(num1) / parseFloat(num2);
-//       break;
-//     default:
-//       updateScreen('Error');
-//       return;
-//   }
-//   updateScreen(result);
-//   num1 = result.toString();
-//   num2 = '';
-//   operator = '';
-// }
-
-// btn1.addEventListener('click', function hola(params) {
-// 	console.log(OperationResult);
-// });
-
-// displayer.textContent = `a text ${OperationResult}`;
